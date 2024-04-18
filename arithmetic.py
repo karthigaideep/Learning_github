984f97128603fcc75681f6d46d52ce646c8b1c8f
karthigai.r <karthigai.r@sophos.com>
2024-04-18 03:21:37 +0000

    checking python code

---
+++ arithmetic.py
@@ -0,0 +1,23 @@
+a = 4
+b = 6
+#testing merge conflit resolution
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)   
